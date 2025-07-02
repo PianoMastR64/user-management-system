7382684f359989f011326f40ea53aa0c53ee78bd
PianoMastR64 <PianoMastR64@gmail.com>
2025-07-02 17:26:57 -0400

    remove @JsonValue annotation and toJson method from Role enum which jsonified to lowercase. enum values should just be capital. keep dejsonification to upper even though UserInputDto handles it as a String

--- src/main/java/com/pianomastr64/usermanagement/user/Role.java
+++ src/main/java/com/pianomastr64/usermanagement/user/Role.java
@@ -1,7 +1,6 @@
 package com.pianomastr64.usermanagement.user;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 
 //TODO consider moving to "role" package if role code gets more complex
 public enum Role {
@@ -11,10 +10,5 @@ public enum Role {
     @JsonCreator
     public static Role fromString(String role) {
         return valueOf(role.toUpperCase());
-    }
-    
-    @JsonValue
-    public String toJson() {
-        return name().toLowerCase();
     }
 }
