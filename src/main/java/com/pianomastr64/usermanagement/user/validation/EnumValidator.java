9629edcd5bf25aa888913c6b8b9d2b260ae29e8d
PianoMastR64 <PianoMastR64@gmail.com>
2025-07-02 17:29:59 -0400

    simplify EnumValidator by removing allowedValues field in favor of Arrays.toString(enumConstants) and improved some formatting

--- src/main/java/com/pianomastr64/usermanagement/user/validation/EnumValidator.java
+++ src/main/java/com/pianomastr64/usermanagement/user/validation/EnumValidator.java
@@ -7,30 +7,25 @@ import java.util.Arrays;
 
 public class EnumValidator implements ConstraintValidator<ValidEnum, String> {
     private Enum<?>[] enumConstants;
-    private String allowedValues;
     
     @Override
     public void initialize(ValidEnum constraintAnnotation) {
         enumConstants = constraintAnnotation.enumClass().getEnumConstants();
-        allowedValues = Arrays.stream(enumConstants)
-            .map(e -> e.name().toLowerCase())
-            .reduce((a, b) -> a + ", " + b)
-            .orElse("");
     }
     
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        if(value == null || value.isEmpty()) {
-            return true; // Allow null or empty values
+        if(value == null) {
+            return true; // Allow null values
         }
         
         boolean valid = Arrays.stream(enumConstants)
             .anyMatch(enumConstant -> enumConstant.name().equalsIgnoreCase(value));
         
-        if (!valid) {
+        if(!valid) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(
-                String.format("Invalid value '%s'. Allowed values are: %s", value, allowedValues))
+                    String.format("Invalid value '%s'. Allowed values are: %s", value.toUpperCase(), Arrays.toString(enumConstants)))
                 .addConstraintViolation();
         }
         
